63bdd2412f47aec473a04d9bee7a5713742686b3
Raphael Vasconcelos <raphael.vasconcelos01@gmail.com>
2019-05-13 18:44:58 -0300

    Começando a separar os Widgets no conceito de paginas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course/product_manager.dart';
+import 'package:flutter_course/pages/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,12 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Easylist'),
-        ),
-        body: ProductManager(),
-      ),
+      home: HomePage()
     );
   }
 }
