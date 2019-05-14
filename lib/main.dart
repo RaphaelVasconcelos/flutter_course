9ad58a5d4ee2df9de39ec8f085f96de5d0a0ff5a
Raphael Vasconcelos <raphael.vasconcelos01@gmail.com>
2019-05-14 18:25:23 -0300

    Adicionanando um tema ao App

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      home: HomePage(),
     );
   }
 }
