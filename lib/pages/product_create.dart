23d3629f8d675a0a6362bcd6fd8aa466f312de80
Raphael Vasconcelos <raphael.vasconcelos01@gmail.com>
2019-05-17 19:48:31 -0300

    Trocando o Widget Column por ListView para ajustar o problema com a falta de espaço

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -16,7 +16,7 @@ class _ProductCreateState extends State<ProductCreatePage> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(10.0),
-      child: Column(
+      child: ListView(
         children: <Widget>[
           TextField(
             decoration: InputDecoration(
@@ -49,6 +49,10 @@ class _ProductCreateState extends State<ProductCreatePage> {
                 priceValue = double.parse(value);
               });
             },
+          ),
+          RaisedButton(
+            child: Text('Save'),
+            onPressed: () {},
           ),
         ],
       ),
