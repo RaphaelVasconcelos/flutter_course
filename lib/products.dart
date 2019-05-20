43082c19f5a1e702ed6621718d51336a224148c4
Raphael Vasconcelos <raphael.vasconcelos01@gmail.com>
2019-05-20 19:50:38 -0300

    Adicionando efeito de price tag

--- lib/products.dart
+++ lib/products.dart
@@ -28,12 +28,17 @@ class Products extends StatelessWidget {
                   SizedBox(
                     width: 8.0,
                   ),
-                  Text(
-                    products[index]['price'].toString(),
-                    style: TextStyle(
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Oswald',
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(5.0)),
+                    child: Text(
+                      '\$${products[index]['price'].toString()}',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ],
