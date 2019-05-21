30f81d02f0002a507018de8d49e6dd137cd52b1e
Raphael Vasconcelos <raphael.vasconcelos01@gmail.com>
2019-05-21 16:37:46 -0300

    Adicionando plano de fundo na tela de autenticação

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -19,7 +19,17 @@ class _AuthPageState extends State<AuthPage> {
         title: Text('Login'),
       ),
       body: Container(
-        margin: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.3),
+              BlendMode.dstATop,
+            ),
+            fit: BoxFit.cover,
+            image: AssetImage('assets/background.jpg'),
+          ),
+        ),
+        padding: EdgeInsets.all(10.0),
         child: ListView(
           children: <Widget>[
             TextField(
