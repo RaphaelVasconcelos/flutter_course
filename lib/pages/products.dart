d005d7957333eace6b85f35e57767ba1008af1ea
Raphael Vasconcelos <raphael.vasconcelos01@gmail.com>
2019-06-10 19:27:34 -0300

    Finalizando funcionalidade de favoritar produtos

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/scoped-models/products.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 import '../widgets/products/products.dart';
 
 class ProductsPage extends StatelessWidget {
-
   Widget _buildSideDrawer(BuildContext context) {
     return Drawer(
       child: Column(
@@ -31,10 +32,15 @@ class ProductsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('EasyList'),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-          )
+          ScopedModelDescendant<ProductsModel>(builder:
+              (BuildContext context, Widget child, ProductsModel model) {
+            return IconButton(
+              icon: Icon(model.displayFavortesOnly ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                model.toggleDisplayMode();
+              },
+            );
+          }),
         ],
       ),
       body: Products(),
